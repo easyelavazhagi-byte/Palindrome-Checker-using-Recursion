73d83d012cf80d18cd37084ae122bafaa8815969
easyelavazhagi-byte <easyelavazhagi@gmail.com>
2026-04-01 09:45:44 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+OUTPUT :
+
+Python 3.14.2 (tags/v3.14.2:df79316, Dec 5 2025, 17:18:21) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+================ RESTART: C:/Users/ELAVAZHAGI/Documents/25.py ================
+Enter a string: Madam
+It is a Palindrome
